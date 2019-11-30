c7efd59dc9a900eda187bb19a125447cec87a22e
CDTH17PMC-LeAnhTuan <55843232+CDTH17PMC-LeAnhTuan@users.noreply.github.com>
2019-11-30 16:35:55 +0700

    Update #7
    Fix regular validation NguoiChoiRequest

--- app/Http/Requests/NguoiChoiRequest.php
+++ app/Http/Requests/NguoiChoiRequest.php
@@ -15,8 +15,7 @@ class NguoiChoiRequest extends FormRequest
     {
         return [
             'ten_dang_nhap' => 'required',
-            'mat_khau' => 'required|min:8|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/',
-            'mat_khau' => 'required',
+            'mat_khau' => 'required|min:6|regex:/^.*(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[#?!@$%^&*-]).*$/',
             'hinh_dai_dien' => 'required',
             'email' => 'required'
         ];
@@ -26,7 +25,7 @@ class NguoiChoiRequest extends FormRequest
         return[
             'ten_dang_nhap.required' => 'Tên đăng nhập không được để trống',
             'mat_khau.required' =>'Mật khẩu không được để trống',
-            'mat_khau.min' => 'Mật khẩu phải ít nhất 8 kí tự',
+            'mat_khau.min' => 'Mật khẩu phải ít nhất 6 kí tự',
             'mat_khau.regex' => 'Mật khẩu phải có ít nhất kí tự đặc biệt và 1 kí tự hoa',
             'hinh_dai_dien.required' =>'Ảnh đại diện không được để trống',
             'email.required' => 'Email không được để trống'
