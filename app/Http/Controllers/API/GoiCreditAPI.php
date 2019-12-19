79ab3e69a2f29ba83ecd02d6e720ca6fb94e8534
tranletanphat-CDTH17PMC <56817092+tranletanphat-CDTH17PMC@users.noreply.github.com>
2019-12-19 23:27:22 +0700

    Cập nhật #10_2
    Cập nhật thêm api danh sách gói credit

---
+++ app/Http/Controllers/API/GoiCreditAPI.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\GoiCreditModel;
+class GoiCreditAPI extends Controller
+{
+    public function LayDSGoiCredit()
+    {
+    	$dsGoiCredit = GoiCreditModel::all();
+    	$result = [
+    		'success' => true,
+    		'data' => $dsGoiCredit
+    	];
+    	return response()->json($result);
+    }
+}
